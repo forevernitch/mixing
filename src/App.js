01193bf0671ce65a3626f700f18b253b9e5f3622
Noel Loera <noel@Noels-MacBook-Pro.local>
2019-11-13 20:24:39 -0700

    11/13/2019: Modified the App.js to contain props for child components

--- src/App.js
+++ src/App.js
@@ -2,32 +2,49 @@ import React from 'react';
 import SearchResults from './Components/SearchResults/SearchResults';
 import SearchBar from './Components/SearchBar/SearchBar';
 import Playlist from './Components/Playlist/Playlist';
-import './App.css'
+import './App.css';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchResults: [
-        {name: "name1", artist: "artist1",album: "album1", id: "1" },
-        { name: "name2", artist: "artist2", album: "album2", id: "2" },
-        { name: "name3", artist: "artist3", album: "album3", id: "3" },
-        { name: "name4", artist: "artist4", album: "album4", id: "4" }
+        {name: "name1", artist: "artist1",album: "album1", id: 1 },
+        { name: "name2", artist: "artist2", album: "album2", id: 2},
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+        { name: "name4", artist: "artist4", album: "album4", id: 4 }
       ],
       playlistName: "Playlist Name",
       playlistTracks:[
-        {name: "song1", artist: "artist1",album: "album1", id: "1" },
-        { name: "song2", artist: "artist2", album: "album2", id: "2" },
-        { name: "song3", artist: "artist3", album: "album3", id: "3" }
+        {name: "song1", artist: "artist1",album: "album1", id: 1},
+        { name: "song2", artist: "artist2", album: "album2", id: 2},
+        { name: "song3", artist: "artist3", album: "album3", id: 3}
       ]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
+  //This will chaeck the current 
   addTrack(track){
-    const checker = this.state.playlistTracks.some(current=> current.id === track.id);
-    if(!checker) this.state.playlistTracks.push(track);
+    const found = this.state.playlistTracks.some(current=> current.id === track.id);
+    if(!found){this.setState({
+      playlistTracks: [...this.state.playlistTracks, track]});
   }
+  //Left off on the addition of the track
+
+  //Effectively removes the object from the array in order to return and set new array
+  removeTrack(track){
+    const removed= this.state.playlistTracks.filter(el =>{
+      return el.id !== track.id;
+    });
+    this.setState({playlistTracks: removed});
+  }
+
+
   
   
   render() {
